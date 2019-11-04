0f734ebafa43dc9fb04ce2b0b5f88c4fccc43ad6
lgdelacruz <bboyjacks@gmail.com>
2019-11-03 17:17:07 -0800

    Added left controls initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./App.css";
+import * as MaterialUI from "@material-ui/core";
+import LeftControls from "./LeftControls";
+import clsx from "clsx";
+
+const useStyles = MaterialUI.makeStyles(theme => {
+  return {
+    app: {
+      display: "flex",
+      height: "100%"
+    },
+    leftControls: {
+      flex: 1
+    },
+    mainPanel: {
+      flex: 3
+    }
+  };
+});
+
+const theme = MaterialUI.createMuiTheme({
+  palette: {
+    primary: MaterialUI.colors.grey,
+    secondary: MaterialUI.colors.deepPurple
+  }
+});
 
 const App = props => {
-  return <div className="App">Hello World</div>;
+  const classes = useStyles();
+  return (
+    <MaterialUI.ThemeProvider theme={theme}>
+      <div className={clsx("App", classes.app)}>
+        <LeftControls className={classes.leftControls} />
+        <div className={classes.mainPanel} />
+      </div>
+    </MaterialUI.ThemeProvider>
+  );
 };
 
 export default App;
