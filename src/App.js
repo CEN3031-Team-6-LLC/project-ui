5c8ddd2b2cc06118683fe0934c40374b122c98c2
lgdelacruz <bboyjacks@gmail.com>
2019-11-03 17:49:02 -0800

    ChartJS sample

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
 import React from "react";
 import "./App.css";
+import * as ChartJS from "react-chartjs-2";
+
+const randomScalingFactor = () => {
+  return Math.random() * 100;
+};
+
+var horizontalBarChartData = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      backgroundColor: "red",
+      borderColor: "lightcoral",
+      borderWidth: 1,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    },
+    {
+      label: "Dataset 2",
+      backgroundColor: "blue",
+      borderColor: "lightgrey",
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    }
+  ]
+};
 
 const App = props => {
-  return <div className="App">Hello World</div>;
+  const dummyRef = React.useRef(null);
+
+  React.useEffect(() => {
+    console.log(dummyRef.current.chartInstance.data);
+  });
+  return (
+    <div className="App">
+      <ChartJS.Line
+        ref={dummyRef}
+        data={horizontalBarChartData}
+        width={100}
+        height={500}
+        options={{ maintainAspectRatio: false }}
+      />
+    </div>
+  );
 };
 
 export default App;
