86e0cf4d51129981d3f9bf54fc2c9c634381cbdf
amakUfl <a.makarevitch@ufl.edu>
2019-11-15 09:07:03 -0500

    facilitating server integration

--- src/post.js
+++ src/post.js
@@ -1,12 +1,21 @@
-const SERVER_URL = "https://quiet-atoll-96617.herokuapp.com";
+const SERVER_URL = "https://secure-ravine-69330.herokuapp.com";
+const UI_URL = "https://sleepy-garden-43138.herokuapp.com";
+const LOCAL_SERVER_URL = "https://quiet-atoll-96617.herokuapp.com";
+const LOCAL_UI_URL = "http://localhost:3000";
 
 export const post = ({ body, type }) => {
+  var servelUrl, uiUrl;
+  if (window.location.hostname === "localhost") {
+    servelUrl = LOCAL_SERVER_URL; uiUrl = LOCAL_UI_URL;
+  } else {
+    servelUrl = SERVER_URL; uiUrl = UI_URL;
+  }
   return new Promise((accept, reject) => {
-    fetch(`${SERVER_URL}/api/calculate/${type}`, {
+    fetch(`${servelUrl}/api/calculate/${type}`, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": uiUrl,
         "Access-Control-Request-Method": "POST"
       },
       body: JSON.stringify(body) //
