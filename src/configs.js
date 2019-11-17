0f152476a8a0e8eaa6baf2e66f00f53274fd6899
amakUfl <a.makarevitch@ufl.edu>
2019-11-17 18:21:12 -0500

    deployment

---
+++ src/configs.js
@@ -0,0 +1,4 @@
+export const configs = {
+    SERVER_URL: window.location.hostname === "localhost" ? "https://quiet-atoll-96617.herokuapp.com" : "https://secure-ravine-69330.herokuapp.com",
+    UI_URL: window.location.hostname === "localhost" ? "http://localhost:3000" : "https://sleepy-garden-43138.herokuapp.com"
+}
--- src/post.js
+++ src/post.js
@@ -1,21 +1,12 @@
-const SERVER_URL = "https://secure-ravine-69330.herokuapp.com";
-const UI_URL = "https://sleepy-garden-43138.herokuapp.com";
-const LOCAL_SERVER_URL = "https://quiet-atoll-96617.herokuapp.com";
-const LOCAL_UI_URL = "http://localhost:3000";
+import { configs } from "./configs";
 
 export const post = ({ body, type }) => {
-  var servelUrl, uiUrl;
-  if (window.location.hostname === "localhost") {
-    servelUrl = LOCAL_SERVER_URL; uiUrl = LOCAL_UI_URL;
-  } else {
-    servelUrl = SERVER_URL; uiUrl = UI_URL;
-  }
   return new Promise((accept, reject) => {
-    fetch(`${servelUrl}/api/calculate/${type}`, {
+    fetch(`${configs.SERVER_URL}/api/calculate/${type}`, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": uiUrl,
+        "Access-Control-Allow-Origin": configs.UI_URL,
         "Access-Control-Request-Method": "POST"
       },
       body: JSON.stringify(body) //
