38a2a5d1298acc923e990c13b077fc31a8048e47
lgdelacruz <bboyjacks@gmail.com>
2019-11-17 17:18:15 -0800

    Added data table

--- src/MainPanel/MainPanel.js
+++ src/MainPanel/MainPanel.js
@@ -1,26 +1,72 @@
 import React from "react";
 import * as ChartJS from "react-chartjs-2";
 import * as MaterialUI from "@material-ui/core";
+import DataTable from "./DataTable";
 
 const useStyles = MaterialUI.makeStyles(theme => {
   return {
     mainPanel: {
       height: "100%"
+    },
+    switch: {
+      display: "flex",
+      justifyContent: "flex-start"
     }
   };
 });
 
 const MainPanel = props => {
-  const { chartData } = props;
+  const { data, chartData } = props;
   const classes = useStyles();
+  const [showChart, setShowChart] = React.useState(false);
   return (
     <div className={classes.mainPanel} {...props}>
-      <ChartJS.Line
-        data={chartData}
-        width={100}
-        height={500}
-        options={{ maintainAspectRatio: false }}
-      />
+      <div className={classes.switch}>
+        <MaterialUI.Switch
+          onChange={e => {
+            setShowChart(e.target.checked);
+          }}
+        />
+      </div>
+      {showChart ? (
+        <ChartJS.Line
+          data={chartData}
+          width={100}
+          height={500}
+          options={{
+            scales: {
+              xAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Distance"
+                  },
+                  type: "logarithmic",
+                  position: "left",
+                  ticks: {
+                    min: 1, //minimum tick
+                    max: 10000, //maximum tick
+                    callback: function(value, index, values) {
+                      return Number(value.toString());
+                    }
+                  },
+                  afterBuildTicks: function(chartObj) {
+                    chartObj.ticks = [];
+                    chartObj.ticks.push(1);
+                    chartObj.ticks.push(10);
+                    chartObj.ticks.push(100);
+                    chartObj.ticks.push(1000);
+                    chartObj.ticks.push(10000);
+                  }
+                }
+              ]
+            },
+            maintainAspectRatio: false
+          }}
+        />
+      ) : (
+        <DataTable data={data} />
+      )}
     </div>
   );
 };
