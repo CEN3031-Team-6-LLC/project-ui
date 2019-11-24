204e464fd449510b88b07d5b34d5adce0175ec7e
lgdelacruz <bboyjacks@gmail.com>
2019-11-24 10:42:08 -0800

    Fixed warning

--- src/GeneralFire/GeneralFire.js
+++ src/GeneralFire/GeneralFire.js
@@ -29,27 +29,26 @@ const GeneralFire = props => {
     windSpeed: {
       amount: 0,
       error: false,
-      errorMessage: "Error: Windspeed must be between 0.1 and 50 m/s",
+      errorMessage: "Error: Windspeed must be between 0.1 and 50 m/s"
     },
 
     receptorHeight: {
       amount: 0,
       error: false,
-      errorMessage: "Error: Receptor Height must be greater than zero",
+      errorMessage: "Error: Receptor Height must be greater than zero"
     },
 
-    fireRadius:{
+    fireRadius: {
       amount: 0,
       error: false,
-      errorMessage: "Error: Radius must be greater than zero",
+      errorMessage: "Error: Radius must be greater than zero"
     },
     stability: "a"
   });
 
   return (
     <div {...props} className={classes.generalFire}>
-
-{/*------------------------------------------ Source Amount*/}
+      {/*------------------------------------------ Source Amount*/}
       <CustomWidgets.InputField
         placeholder="Source Amount"
         unit="Ci"
@@ -58,7 +57,6 @@ const GeneralFire = props => {
         title="Source Amount"
         type="number"
         data={state.sourceAmount}
-
         onChange={e => {
           const value = window.parseInt(e.target.value);
           if (value >= 0) {
@@ -70,7 +68,7 @@ const GeneralFire = props => {
         }}
       />
 
- {/*------------------------------------------ Fire Cloud Top*/}
+      {/*------------------------------------------ Fire Cloud Top*/}
       <CustomWidgets.InputField
         placeholder="Fire Cloud Top"
         unit="m"
@@ -79,7 +77,6 @@ const GeneralFire = props => {
         title="Fire Cloud Top"
         type="number"
         data={state.fireCloudTop}
-
         onChange={e => {
           const value = window.parseInt(e.target.value);
           if (value >= 0) {
@@ -87,11 +84,11 @@ const GeneralFire = props => {
           } else {
             state.fireCloudTop.error = true;
           }
-          setState({ ...state});
+          setState({ ...state });
         }}
       />
 
-{/*----------------------------------------------- Wind Speed*/}
+      {/*----------------------------------------------- Wind Speed*/}
       <CustomWidgets.InputField
         placeholder="Wind Speed"
         unit="m/s"
@@ -100,33 +97,33 @@ const GeneralFire = props => {
         title="Wind Speed"
         type="number"
         data={state.windSpeed}
-
         onChange={e => {
           const value = window.parseFloat(e.target.value);
-        
-          // Condition mph
-          if (state.unitTogglelable == true){//<<------- This line needs fix
 
-            if(value >= 0.2 && value <= 110){
+          // Condition mph
+          if (state.unitTogglelable === true) {
+            //<<------- This line needs fix
+
+            if (value >= 0.2 && value <= 110) {
               state.fireCloudTop.amount = value;
-            }else{
+            } else {
               state.windSpeed.error = true;
-              state.windSpeed.errorMessage = "Error: Value must be between 0.2 and 110 mph";
+              state.windSpeed.errorMessage =
+                "Error: Value must be between 0.2 and 110 mph";
             }
 
-          // Condition m/s
-          }else if(value >= 0.1 && value <= 50){  
+            // Condition m/s
+          } else if (value >= 0.1 && value <= 50) {
             state.windSpeed.amount = value;
-          }else{
+          } else {
             state.windSpeed.error = true;
-
           }
 
-          setState({ ...state});
+          setState({ ...state });
         }}
       />
 
-{/*----------------------------------------------- Receptor Height*/}
+      {/*----------------------------------------------- Receptor Height*/}
       <CustomWidgets.InputField
         placeholder="Receptor Height"
         unit="m"
@@ -135,7 +132,6 @@ const GeneralFire = props => {
         title="Receptor Height"
         type="number"
         data={state.receptorHeight}
-
         onChange={e => {
           const value = window.parseInt(e.target.value);
           if (value >= 0) {
@@ -143,11 +139,11 @@ const GeneralFire = props => {
           } else {
             state.receptorHeight.error = true;
           }
-          setState({ ...state});
+          setState({ ...state });
         }}
       />
 
-{/*--------------------------------------------------- Fire Radius*/}
+      {/*--------------------------------------------------- Fire Radius*/}
       <CustomWidgets.InputField
         placeholder="Fire Radius"
         unit="m"
@@ -156,7 +152,6 @@ const GeneralFire = props => {
         title="Fire Radius"
         type="number"
         data={state.fireRadius}
-
         onChange={e => {
           const value = window.parseInt(e.target.value);
           if (value >= 0) {
@@ -164,11 +159,11 @@ const GeneralFire = props => {
           } else {
             state.fireRadius.error = true;
           }
-          setState({ ...state});
+          setState({ ...state });
         }}
       />
 
-{/*--------------------------------------------------- Stability*/}
+      {/*--------------------------------------------------- Stability*/}
       <CustomWidgets.RadioButtons
         title="Stability"
         options={[
