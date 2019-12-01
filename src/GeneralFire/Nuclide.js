a93562429f90c7f9a83085645e232a53c73d4672
lgdelacruz <bboyjacks@gmail.com>
2019-11-30 18:02:20 -0800

    Nuclide basics

--- src/GeneralFire/Nuclide.js
+++ src/GeneralFire/Nuclide.js
@@ -2,28 +2,48 @@ import React from "react";
 import * as MaterialUI from "@material-ui/core";
 import { configs } from "../configs";
 
+const useStyles = MaterialUI.makeStyles(theme => {
+  return {
+    nuclide: {
+      display: "block",
+      width: "100%",
+      marginBottom: 20
+    },
+    label: {
+      transform: "translate(10px, 20px)"
+    }
+  };
+});
+
 export default function Nuclide(props) {
+  const classes = useStyles();
   const age = 20;
   const handleChange = e => {};
 
   React.useEffect(() => {
     fetch(`${configs.SERVER_URL}/api/nuclides/getNuclideList`)
       .then(resp => {
-        console.log("Nuclide results", resp);
+        return resp.text();
       })
+      .then(text => console.log(text))
       .catch(e => console.log("Error", e));
   }, []);
 
   return (
-    <MaterialUI.FormControl>
-      <MaterialUI.InputLabel id="demo-customized-select-label">
+    <MaterialUI.FormControl className={classes.nuclide}>
+      <MaterialUI.InputLabel
+        className={classes.label}
+        id="demo-customized-select-label"
+      >
         Nuclides
       </MaterialUI.InputLabel>
       <MaterialUI.Select
+        variant="outlined"
         labelId="demo-customized-select-label"
         id="demo-customized-select"
         value={age}
         onChange={handleChange}
+        style={{ width: "100%" }}
       ></MaterialUI.Select>
     </MaterialUI.FormControl>
   );
