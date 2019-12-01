8e4ca27d891055756e7d3c93a2d278121d12b442
lgdelacruz <bboyjacks@gmail.com>
2019-12-01 11:49:32 -0800

    Max distance and distance increment key strings

--- src/General/KeyStrings.js
+++ src/General/KeyStrings.js
@@ -3,5 +3,7 @@ export const KeyStrings = {
   fireCloudTop: "Fire Cloud Top",
   windSpeed: "Wind Speed",
   receptorHeight: "Receptor Height",
-  fireRadius: "Fire Radius"
+  fireRadius: "Fire Radius",
+  maxDistance: "Max Distance",
+  distanceIncrement: "Distance Increment"
 };
