ecd8f247ee01e487f99696490c216df8c5cff8fe
lgdelacruz <bboyjacks@gmail.com>
2019-12-01 17:18:40 -0800

    checking if isotope is valid first before sending request

--- src/GeneralFire/LungClass.js
+++ src/GeneralFire/LungClass.js
@@ -6,23 +6,27 @@ const LungClass = props => {
   const { isotope, onChange, icrp } = props;
   const [options, setOptions] = React.useState([]);
   React.useEffect(() => {
-    let lungClassUrl = `${configs.SERVER_URL}/api/nuclides/getNuclidesLungClasses/${isotope}`;
-    if (icrp) {
-      lungClassUrl = `${configs.SERVER_URL}/api/nuclides/getNuclidesICRPLungClass/${isotope}`;
+    if (isotope.length > 0) {
+      let lungClassUrl = `${configs.SERVER_URL}/api/nuclides/getNuclidesLungClasses/${isotope}`;
+      if (icrp) {
+        lungClassUrl = `${configs.SERVER_URL}/api/nuclides/getNuclidesICRPLungClass/${isotope}`;
+      }
+      fetch(lungClassUrl)
+        .then(resp => {
+          return resp.text();
+        })
+        .then(text => {
+          let textArr;
+          if (icrp) {
+            setOptions([{ value: text, label: text }]);
+          } else {
+            textArr = JSON.parse(text);
+            const newOptions = textArr.map(t => ({ value: t, label: t }));
+            setOptions(newOptions);
+          }
+        })
+        .catch(e => console.log("you are the one", e));
     }
-    fetch(lungClassUrl)
-      .then(resp => resp.text())
-      .then(text => {
-        let textArr;
-        if (icrp) {
-          setOptions([{ value: text, label: text }]);
-        } else {
-          textArr = JSON.parse(text);
-          const newOptions = textArr.map(t => ({ value: t, label: t }));
-          setOptions(newOptions);
-        }
-      })
-      .catch(e => console.log(e));
   }, [isotope, icrp]);
   return (
     <RadioButtons
